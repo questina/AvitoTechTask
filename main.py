3cbbd62d768858f726e0bcda53c557b951c2856d
questina <chrismatis123@gmail.com>
2021-09-10 10:54:52 +0300

    added tests and comments

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-import asyncio
-from math import sqrt
-
 import aiohttp
 from typing import List, Tuple
 
-SOURCE_URL = 'https://raw.githubusercontent.com/avito-tech/python-trainee-assignment/main/matrix.txt'
-
 
 def read_matrix(raw_matr: str) -> Tuple[List[int], int]:
+    """
+    Функция возвращает массив из целых неотрицательных чисел, полученный из входной строки.
+
+    Совместно с матрицей, эта функция находит одно из измерений матрицы. Последнее значение можно было найти
+    с помощью функции sqrt из библиотеки math, но в задании не было указана возможность использования
+    дополнительных библиотек, поэтому автор решила обойтись без нее.
+    """
     matr: List[int] = list()
     matr_dim: int = 0
+
     raw_matr = raw_matr.replace('+', '').replace('-', '').replace(' ', '')
     for row in raw_matr.split('\n'):
         if row == '':
@@ -20,33 +23,41 @@ def read_matrix(raw_matr: str) -> Tuple[List[int], int]:
 
 
 def zig_zag_change(matrix: List[int], matr_dim: int) -> List[int]:
-    iter_num: int = 1
-    cur_i: int = 0
+    """
+    Функция возвращает список, содержащий результат обхода матрицы по спирали:
+    против часовой стрелки, начиная с левого верхнего угла.
+    """
+    if matr_dim < 2:
+        return matrix
+
+    iter_num: int = 1  # номер начальной итерации. За итерацию считается сдвиг влево или вправо + сдвиг вниз или вверх
+    cur_i: int = 0  # индекс текущего элемента
     res_matr: List[int] = list()
+
+    # Спускаемся вниз до последней строки
     res_matr.append(matrix[cur_i])
-    for i in range(matr_dim-iter_num):
+    for i in range(matr_dim - iter_num):
         cur_i += matr_dim
         res_matr.append(matrix[cur_i])
+
+    # Основной цикл
     while iter_num != matr_dim:
-        for i in range(matr_dim-iter_num):
-            cur_i += 1 if iter_num%2 == 1 else -1
+        for i in range(matr_dim - iter_num):
+            cur_i += 1 if iter_num % 2 == 1 else -1
             res_matr.append(matrix[cur_i])
-        for i in range(matr_dim-iter_num):
-            cur_i -= matr_dim if iter_num%2 == 1 else -matr_dim
+        for i in range(matr_dim - iter_num):
+            cur_i -= matr_dim if iter_num % 2 == 1 else -matr_dim
             res_matr.append(matrix[cur_i])
         iter_num += 1
+
     return res_matr
 
 
 async def get_matrix(url: str) -> List[int]:
     async with aiohttp.ClientSession(
-            connector=aiohttp.TCPConnector(ssl=False)  # HTTP Connection
+            connector=aiohttp.TCPConnector(ssl=False)
     ) as session:
         async with session.get(url=url) as response:
             data = await response.text()
             matr, matr_dim = read_matrix(data)
     return zig_zag_change(matr, matr_dim)
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(get_matrix(SOURCE_URL))
--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
 import asyncio
 
-from main import get_matrix
+from main import get_matrix, zig_zag_change
 
 SOURCE_URL = 'https://raw.githubusercontent.com/avito-tech/python-trainee-assignment/main/matrix.txt'
+
 TRAVERSAL = [
     10, 50, 90, 130,
     140, 150, 160, 120,
@@ -15,4 +16,40 @@ def test_get_matrix():
     assert asyncio.run(get_matrix(SOURCE_URL)) == TRAVERSAL
 
 
-test_get_matrix()
+def test_zig_zag_algorithm_5():
+    matr = [i * 10 for i in range(1, 26)]
+    trav_matr = [10, 60, 110, 160, 210,
+                 220, 230, 240, 250, 200,
+                 150, 100, 50, 40, 30,
+                 20, 70, 120, 170, 180,
+                 190, 140, 90, 80, 130]
+    assert zig_zag_change(matr, 5) == trav_matr
+
+
+def test_zig_zag_algorithm_1():
+    matr = [1]
+    trav_matr = [1]
+    assert zig_zag_change(matr, 0) == trav_matr
+
+
+def test_zig_zag_algoritm_3():
+    matr = [i * 10 for i in range(1, 10)]
+    trav_matr = [10, 40, 70,
+                 80, 90, 60,
+                 30, 20, 50]
+    assert zig_zag_change(matr, 3) == trav_matr
+
+
+def test_zig_zag_algorithm_2():
+    matr = [i * 10 for i in range(1, 5)]
+    trav_matr = [10, 30,
+                 40, 20]
+    assert zig_zag_change(matr, 2) == trav_matr
+
+
+def run_tests():
+    test_get_matrix()
+    test_zig_zag_algorithm_1()
+    test_zig_zag_algorithm_2()
+    test_zig_zag_algoritm_3()
+    test_zig_zag_algorithm_5()
